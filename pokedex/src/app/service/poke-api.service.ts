d5c96f8e4b29f0efb8aa9ce4ac8bde37281f2367
ThiSBernardo <93749791+ThiSBernardo@users.noreply.github.com>
2022-06-02 08:37:38 -0300

    fim

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 //Observable
 import { Observable } from 'rxjs';
@@ -16,23 +16,23 @@ export class PokeApiService {
     private http: HttpClient
   ) { }
 
-  get apiListAllPokemons(): Observable<any> {
+  get apiListAllPokemons():Observable<any>{
     return this.http.get<any>(this.url).pipe(
-      tap(res => res),
-      tap(res => {
-        res.results.map((resPokemons: any) => {
+      tap( res => res ),
+      tap( res => {
+        res.results.map( (resPokemons: any) => {
 
           this.apiGetPokemon(resPokemons.url).subscribe(
             res => resPokemons.status = res
-      )
+          );
 
         })
       })
     )
   }
 
-  public apiGetPokemon(url: string) : Observable<any>{
-   return this.http.get<any>(url).pipe(
+  public apiGetPokemon( url: string ):Observable<any>{
+    return this.http.get<any>( url ).pipe(
       map(
         res => res
       )
